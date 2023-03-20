3727be5657f0dd9114bafb1158dea7a80385b426
Brennan McMicking <brennanmcmicking@gmail.com>
2023-03-20 13:26:50 -0700

    v1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # eee-ticket-maker
+
+Produces a n*2-page PDF of alternating front/back EEE tickets with the ticket numbers automatically populated using the font in `font.ttf`. It creates a PDF in this way because that's the format that Zapcopy accepts for printing.
+
+## Usage
+
+`python3 number.py` or `python3 number.py <n>` where `n` is the number of tickets to put in the PDF.
---
+++ number.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import sys
+
+if len(sys.argv) < 2:
+    n = 150
+else:
+    n = int(sys.argv[1])
+
+front = Image.open("front.png").convert("RGB")
+reverse = Image.open("reverse.png").convert("RGB")
+
+font = ImageFont.truetype("font.ttf", 72)
+
+images = []
+
+for i in range(1, 151):
+    r = reverse.copy()
+    reverse_draw = ImageDraw.Draw(r)
+    reverse_draw.text((70, 425), f"#{i:03}", (0, 0, 0), font=font)
+    images.append(front)
+    images.append(r)
+
+images[0].save(
+    "output.pdf", "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
+)
+
+front.close()
+reverse.close()
