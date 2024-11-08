c73cefbfcd19860c3843e98a1a7c774d5dc2dc86
Brennan McMicking <brennanmcmicking@gmail.com>
2024-11-07 19:17:37 -0800

    archiving

--- number.py
+++ number.py
@@ -9,22 +9,33 @@ else:
     n = int(sys.argv[1])
 
 front = Image.open("front.png").convert("RGB")
-reverse = Image.open("reverse.png").convert("RGB")
+reverse = Image.open("reverse.png").convert("RGBA")
 
-font = ImageFont.truetype("font.otf", 72)
+font = ImageFont.truetype("DMSerifDisplay-Regular.ttf", 72)
 
 images = []
 
 for i in range(1, n + 1):
-    r = reverse.copy()
-    reverse_draw = ImageDraw.Draw(r)
-    reverse_draw.text((70, 425), f"#{i:03}", (0, 0, 0), font=font)
+    print(i)
+    reverse_text = Image.new("RGBA", reverse.size, (255, 255, 255, 0))
+
+    reverse_draw = ImageDraw.Draw(reverse_text)
+    reverse_draw.text((80, 435), f"#{i:03}", (163, 67, 104, 50), font=font)
+    reverse_draw.text((75, 430), f"#{i:03}", (163, 67, 104, 100), font=font)
+    reverse_draw.text((70, 425), f"#{i:03}", (255, 255, 255), font=font)
+
+    r = Image.alpha_composite(reverse, reverse_text).convert("RGB")
+
     images.append(front)
     images.append(r)
 
+print("saving pdf")
 images[0].save(
     "output.pdf", "PDF", resolution=100.0, save_all=True, append_images=images[1:]
 )
+print("done saving pdf")
 
 front.close()
 reverse.close()
+
+print("done everything")
